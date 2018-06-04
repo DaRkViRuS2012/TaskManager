a9d0f1c198826befe925996d7d84f8b2b17e75a7
DaRkViRuS2012 <nour_araar_2009@yahoo.com>
2018-06-04 18:41:11 +0300

    add title to lap

---
+++ TaskManager/LapCell.swift
@@ -0,0 +1,24 @@
+//
+//  LapCell.swift
+//  TaskManager
+//
+//  Created by Nour  on 6/4/18.
+//  Copyright © 2018 NourAraar. All rights reserved.
+//
+
+import UIKit
+
+class LapCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
